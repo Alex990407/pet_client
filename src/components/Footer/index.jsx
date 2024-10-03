6b483fc20980a332681f24d7fab111540de73d4c
Alex990407 <zhelezniako07@gmail.com>
2024-10-03 12:24:40 +0200

    footer is done

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import instagramIcon from "../../assets/icons/instagram.svg";
+import whatsappIcon from "../../assets/icons/whatsapp.svg";
 import mapImage from "../../assets/map.png"; // Импорт изображения карты
 
 const Footer = () => {
@@ -23,69 +25,180 @@ const Footer = () => {
         }}
       >
         {/* Contact Section */}
-        <Box sx={{ flex: 1, alignSelf: "flex-start", paddingTop: "40px" }}>
-          <Typography variant="h4" gutterBottom>
+        <Box sx={{ flex: 1, paddingLeft: "40px", paddingTop: "40px" }}>
+          <Typography variant="h4" gutterBottom sx={{ textAlign: "left" }}>
             Contact
           </Typography>
         </Box>
 
         {/* Phone and Socials Section */}
-        <Box sx={{ flex: 1, display: "flex", justifyContent: "space-between" }}>
-          {/* Phone */}
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column", // Изменяется на столбец на малых экранах
+            justifyContent: "space-between",
+            gap: "16px", // Промежуток между элементами
+            "@media (min-width: 600px)": {
+              flexDirection: "row", // На больших экранах отображаем в строку
+            },
+          }}
+        >
+          {/* Phone Section */}
           <Box
             sx={{
               marginBottom: "16px",
-              backgroundColor: "rgba(241, 243, 244, 1)", // Установленный цвет
+              backgroundColor: "rgba(241, 243, 244, 1)", // Цвет фона
               padding: "16px",
               borderRadius: "8px",
+              width: "780px", // Ширина контейнера
+              maxWidth: "780px", // Максимальная ширина контейнера
+              margin: "0",
+              textAlign: "left", // Выровнять текст по левому краю
+              "@media (max-width: 600px)": {
+                padding: "12px", // Уменьшенный отступ для малых экранов
+              },
             }}
           >
-            <Typography variant="body1">Phone</Typography>
-            <Typography variant="h6" color="textPrimary">
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: "Montserrat, sans-serif", // Применение шрифта Montserrat
+                color: "rgba(139, 139, 139, 1)", // Цвет текста
+              }}
+            >
+              Phone
+            </Typography>
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              sx={{ fontFamily: "Montserrat, sans-serif", fontWeight: 700 }} // Применение шрифта Montserrat
+            >
               +49 30 915-88492
             </Typography>
           </Box>
 
-          {/* Socials */}
+          {/* Socials Section */}
+
           <Box
             sx={{
-              backgroundColor: "rgba(241, 243, 244, 1)", // Установленный цвет
+              marginBottom: "16px",
+              backgroundColor: "rgba(241, 243, 244, 1)", // Цвет фона
               padding: "16px",
               borderRadius: "8px",
+              width: "548px", // Ширина контейнера
+              maxWidth: "548px", // Максимальная ширина контейнера
+              margin: "0", // Убираем центрирование
+              textAlign: "left", // Выровнять текст по левому краю
+              "@media (max-width: 600px)": {
+                padding: "12px", // Уменьшенный отступ для малых экранов
+              },
             }}
           >
-            <Typography variant="body1">Socials</Typography>
-            {/* Место для иконок социальных сетей, если нужно */}
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: "Montserrat, sans-serif", // Применение шрифта Montserrat
+                color: "rgba(139, 139, 139, 1)", // Цвет текста
+              }}
+            >
+              Socials
+            </Typography>
+            <Box sx={{ display: "flex", gap: "16px", marginTop: "8px" }}>
+              <img
+                src={instagramIcon}
+                alt="Instagram"
+                style={{ width: "24px", height: "24px", cursor: "pointer" }} // Размер иконки
+              />
+              <img
+                src={whatsappIcon}
+                alt="WhatsApp"
+                style={{ width: "24px", height: "24px", cursor: "pointer" }} // Размер иконки
+              />
+            </Box>
           </Box>
         </Box>
 
         {/* Address and Working Hours Section */}
-        <Box sx={{ flex: 1, display: "flex", justifyContent: "space-between" }}>
-          {/* Address */}
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column", // Изменяется на столбец на малых экранах
+            justifyContent: "space-between",
+            gap: "16px", // Промежуток между элементами
+            "@media (min-width: 600px)": {
+              flexDirection: "row", // На больших экранах отображаем в строку
+            },
+          }}
+        >
+          {/* Address Section */}
           <Box
             sx={{
               marginBottom: "16px",
-              backgroundColor: "rgba(241, 243, 244, 1)", // Установленный цвет
+              backgroundColor: "rgba(241, 243, 244, 1)", // Цвет фона
               padding: "16px",
               borderRadius: "8px",
+              width: "780px", // Ширина контейнера
+              maxWidth: "780px", // Максимальная ширина контейнера
+              margin: "0",
+              textAlign: "left",
+              "@media (max-width: 600px)": {
+                padding: "12px", // Уменьшенный отступ для малых экранов
+              },
             }}
           >
-            <Typography variant="body1">Address</Typography>
-            <Typography variant="h6" color="textPrimary">
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: "Montserrat, sans-serif", // Применение шрифта Montserrat
+                color: "rgba(139, 139, 139, 1)", // Цвет текста
+              }}
+            >
+              Address
+            </Typography>
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              sx={{ fontFamily: "Montserrat, sans-serif", fontWeight: 700 }} // Применение шрифта Montserrat
+            >
               Wallstraße 9-13, 10179 Berlin, Deutschland
             </Typography>
           </Box>
 
-          {/* Working Hours */}
+          {/* Working Hours Section */}
           <Box
             sx={{
-              backgroundColor: "rgba(241, 243, 244, 1)", // Установленный цвет
+              marginBottom: "16px",
+              backgroundColor: "rgba(241, 243, 244, 1)", // Цвет фона
               padding: "16px",
               borderRadius: "8px",
+              width: "548px", // Ширина контейнера
+              maxWidth: "548px", // Максимальная ширина контейнера
+              margin: "0",
+              textAlign: "left",
+              "@media (max-width: 600px)": {
+                padding: "12px", // Уменьшенный отступ для малых экранов
+              },
             }}
           >
-            <Typography variant="body1">Working Hours</Typography>
-            <Typography variant="h6" color="textPrimary">
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: "Montserrat, sans-serif", // Применение шрифта Montserrat
+                color: "rgba(139, 139, 139, 1)", // Цвет текста
+              }}
+            >
+              Working Hours
+            </Typography>
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              sx={{
+                fontFamily: "Montserrat, sans-serif",
+                fontWeight: 700,
+              }}
+            >
               24 hours a day
             </Typography>
           </Box>
@@ -105,94 +218,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// ===================================================================
-
-// const Footer = () => {
-//   return (
-//     <Box
-//       sx={{
-//         backgroundColor: "white",
-//         padding: "32px 0",
-//         borderTop: "2px solid #ccc", // Верхняя граница
-//       }}
-//     >
-//       <Box
-//         sx={{
-//           display: "flex",
-//           flexDirection: "column",
-//           justifyContent: "space-between",
-//           maxWidth: "1200px",
-//           margin: "0 auto",
-//           padding: "0 16px",
-//           gap: "24px",
-//         }}
-//       >
-//         {/* Contact Section */}
-
-//         <Box
-//           sx={{
-//             flex: 1,
-//             alignSelf: "flex-start",
-//             paddingLeft: "20px",
-//             paddingTop: "40px",
-//           }}
-//         >
-//           <Typography variant="h4" gutterBottom>
-//             Contact
-//           </Typography>
-//         </Box>
-
-//         {/* Phone and Socials Section */}
-//         <Box
-//           sx={{
-//             flex: 1,
-//             display: "flex",
-//             justifyContent: "space-between",
-//             backgroundColor: "rgba(241, 243, 244, 1)",
-//             gap: "16px",
-//             padding: "32px",
-//           }}
-//         >
-//           <Box sx={{ marginBottom: "16px" }}>
-//             <Typography variant="body1">Phone</Typography>
-//             <Typography variant="h6" color="textPrimary">
-//               +49 30 915-88492
-//             </Typography>
-//           </Box>
-//           <Box>
-//             <Typography variant="body1">Socials</Typography>
-//             {/* Место для иконок социальных сетей, если нужно */}
-//           </Box>
-//         </Box>
-
-//         {/* Address and Working Hours Section */}
-//         <Box sx={{ flex: 1, display: "flex", justifyContent: "space-between" }}>
-//           <Box sx={{ marginBottom: "16px" }}>
-//             <Typography variant="body1">Address</Typography>
-//             <Typography variant="h6" color="textPrimary">
-//               Wallstraße 9-13, 10179 Berlin, Deutschland
-//             </Typography>
-//           </Box>
-//           <Box>
-//             <Typography variant="body1">Working Hours</Typography>
-//             <Typography variant="h6" color="textPrimary">
-//               24 hours a day
-//             </Typography>
-//           </Box>
-//         </Box>
-
-//         {/* Map Section */}
-//         <Box sx={{ flex: 1 }}>
-//           <img
-//             src={mapImage}
-//             alt="Google Maps Location"
-//             style={{ width: "100%", height: "auto" }}
-//           />
-//         </Box>
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default Footer;
