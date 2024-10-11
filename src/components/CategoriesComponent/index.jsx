24d983e7e0a1828d7ee13eb1d457a54366ec6edd
Alex990407 <zhelezniako07@gmail.com>
2024-10-11 08:50:06 +0200

    product filter component styled

--- src/components/CategoriesComponent/index.jsx
+++ src/components/CategoriesComponent/index.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Card, CardMedia, CardContent, Grid } from "@mui/material";
-import axios from "axios"; // Импортируем Axios
-// import ContWithButton from "../TitleLineButton";
+import {
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  Grid,
+  Box,
+} from "@mui/material";
+import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 
 const CategoriesComponent = ({ limit }) => {
@@ -30,49 +37,67 @@ const CategoriesComponent = ({ limit }) => {
   };
 
   return (
-    <Grid container spacing={2}>
-      {categories.map((category) => (
-        <Grid item key={category.id} xs={12} sm={6} md={3}>
-          <Card
-            sx={{
-              boxShadow: "none", // Убираем границы и тени
-              textAlign: "center", // Центрируем содержимое
-              cursor: "pointer",
-            }}
-            onClick={() => handleCategoryClick(category.id)} // Добавляем обработчик клика
-          >
-            <CardMedia
-              component="img"
-              image={"http://localhost:3333/" + category.image} // URL изображения категории
-              alt={category.title}
+    <Box sx={{ padding: "20px" }}>
+      {/* Заголовок страницы */}
+      <Typography
+        variant="h4"
+        component="h2"
+        sx={{
+          fontFamily: "Montserrat, sans-serif",
+          fontWeight: "bold",
+          fontSize: { xs: "1.5rem", sm: "2rem" }, // Адаптивный заголовок
+          marginBottom: "20px",
+          textAlign: "left",
+        }}
+      >
+        Categories
+      </Typography>
+
+      {/* Сетка с категориями */}
+      <Grid container spacing={2}>
+        {categories.map((category) => (
+          <Grid item key={category.id} xs={12} sm={6} md={3}>
+            <Card
               sx={{
-                borderRadius: "8px",
-                objectFit: "contain", // Изображение не обрезается и полностью вмещается
-                objectPosition: "center", // Центрируем изображение
-                margin: "0 auto", // Центрируем элемент
-                backgroundColor: "#f0f0f0", // Цвет фона
-                maxHeight: "350px", // Ограничиваем максимальную высоту
-                width: "100%",
+                boxShadow: "none", // Убираем границы и тени
+                textAlign: "center", // Центрируем содержимое
+                cursor: "pointer",
               }}
-            />
-            <CardContent>
-              <Typography
-                variant="h6"
-                component="div"
+              onClick={() => handleCategoryClick(category.id)} // Добавляем обработчик клика
+            >
+              <CardMedia
+                component="img"
+                image={"http://localhost:3333/" + category.image} // URL изображения категории
+                alt={category.title}
                 sx={{
-                  fontFamily: "Montserrat, sans-serif", // Применяем шрифт
-                  fontWeight: 500, // Устанавливаем Medium вес шрифта
-                  fontSize: "1rem", // Размер текста
-                  marginTop: "10px", // Отступ сверху
+                  borderRadius: "8px",
+                  objectFit: "contain", // Изображение не обрезается и полностью вмещается
+                  objectPosition: "center", // Центрируем изображение
+                  margin: "0 auto", // Центрируем элемент
+                  backgroundColor: "#f0f0f0", // Цвет фона
+                  maxHeight: "350px", // Ограничиваем максимальную высоту
+                  width: "100%",
                 }}
-              >
-                {category.title}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+              />
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{
+                    fontFamily: "Montserrat, sans-serif", // Применяем шрифт
+                    fontWeight: 500, // Устанавливаем Medium вес шрифта
+                    fontSize: "1rem", // Размер текста
+                    marginTop: "10px", // Отступ сверху
+                  }}
+                >
+                  {category.title}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
--- src/components/ProductFilterComponent/index.jsx
+++ src/components/ProductFilterComponent/index.jsx
@@ -34,9 +34,9 @@ const ProductFilterComponent = ({ onFilter }) => {
         maxWidth: "910px",
         display: "flex",
         alignItems: "center",
-        gap: 2,
+        gap: 1,
         flexWrap: "wrap", // Для мобильных устройств элементы могут переходить на следующую строку
-        padding: "16px",
+        padding: "10px",
         marginBottom: "40px",
         // Для мобильных устройств меняем направление на столбец
         "@media (max-width: 600px)": {
@@ -48,13 +48,18 @@ const ProductFilterComponent = ({ onFilter }) => {
     >
       <Typography
         variant="body1"
-        sx={{ marginRight: "8px", fontFamily: "Montserrat", fontWeight: 600 }}
+        sx={{
+          marginRight: "8px",
+          fontFamily: "Montserrat",
+          fontSize: "24px",
+          fontWeight: 600,
+        }}
       >
-        Price:
+        Price
       </Typography>
 
       <TextField
-        label="From"
+        label="from"
         type="number"
         value={priceFrom}
         onChange={(e) => setPriceFrom(e.target.value)}
@@ -64,7 +69,7 @@ const ProductFilterComponent = ({ onFilter }) => {
       />
 
       <TextField
-        label="To"
+        label="to"
         type="number"
         value={priceTo}
         onChange={(e) => setPriceTo(e.target.value)}
@@ -75,9 +80,14 @@ const ProductFilterComponent = ({ onFilter }) => {
 
       <Typography
         variant="body1"
-        sx={{ marginLeft: "16px", fontFamily: "Montserrat", fontWeight: 600 }}
+        sx={{
+          marginLeft: "16px",
+          fontFamily: "Montserrat",
+          fontSize: "24px",
+          fontWeight: 600,
+        }}
       >
-        Discounted items:
+        Discounted items
       </Typography>
 
       <Checkbox
@@ -88,9 +98,14 @@ const ProductFilterComponent = ({ onFilter }) => {
 
       <Typography
         variant="body1"
-        sx={{ marginLeft: "16px", fontFamily: "Montserrat", fontWeight: 600 }}
+        sx={{
+          marginLeft: "16px",
+          fontFamily: "Montserrat",
+          fontSize: "24px",
+          fontWeight: 600,
+        }}
       >
-        Sorted:
+        Sorted
       </Typography>
 
       <FormControl
@@ -104,7 +119,7 @@ const ProductFilterComponent = ({ onFilter }) => {
           },
         }}
       >
-        <InputLabel id="sort-label">Sort By</InputLabel>
+        <InputLabel id="sort-label">Sort</InputLabel>
         <Select
           labelId="sort-label"
           value={sortOrder}
@@ -119,81 +134,6 @@ const ProductFilterComponent = ({ onFilter }) => {
         </Select>
       </FormControl>
     </Box>
-    // <Box
-    //   sx={{
-    //     maxWidth: "910px",
-    //     maxHeight: "36px",
-    //     display: "flex", // Flexbox для горизонтального расположения
-    //     alignItems: "center", // Выровнять элементы по центру по вертикали
-    //     gap: 2, // Расстояние между элементами
-    //     flexWrap: "wrap", // Для мобильных устройств элементы могут переходить на следующую строку
-    //     padding: "16px",
-    //     marginBottom: "40px",
-    //   }}
-    // >
-    //   <Typography
-    //     variant="body1"
-    //     sx={{ marginRight: "8px", fontFamily: "Montserrat", fontWeight: 600 }} // Добавили нужные стили
-    //   >
-    //     Price:
-    //   </Typography>
-
-    //   <TextField
-    //     label="From"
-    //     type="number"
-    //     value={priceFrom}
-    //     onChange={(e) => setPriceFrom(e.target.value)}
-    //     variant="outlined"
-    //     size="small"
-    //     sx={{ width: "100px", height: "30px" }}
-    //   />
-
-    //   <TextField
-    //     label="To"
-    //     type="number"
-    //     value={priceTo}
-    //     onChange={(e) => setPriceTo(e.target.value)}
-    //     variant="outlined"
-    //     size="small"
-    //     sx={{ width: "100px", marginLeft: "8px", height: "30px" }}
-    //   />
-
-    //   <Typography
-    //     variant="body1"
-    //     sx={{ marginLeft: "16px", fontFamily: "Montserrat", fontWeight: 600 }}
-    //   >
-    //     Discounted items:
-    //   </Typography>
-
-    //   <Checkbox
-    //     checked={showDiscounted}
-    //     onChange={(e) => setShowDiscounted(e.target.checked)}
-    //     sx={{ marginLeft: "8px" }}
-    //   />
-
-    //   <Typography
-    //     variant="body1"
-    //     sx={{ marginLeft: "16px", fontFamily: "Montserrat", fontWeight: 600 }} // Добавили нужные стили
-    //   >
-    //     Sorted:
-    //   </Typography>
-
-    //   <FormControl variant="outlined" size="small" sx={{ minWidth: "150px" }}>
-    //     <InputLabel id="sort-label">Sort By</InputLabel>
-    //     <Select
-    //       labelId="sort-label"
-    //       value={sortOrder}
-    //       onChange={(e) => setSortOrder(e.target.value)}
-    //       label="Sort By"
-    //     >
-    //       <MenuItem value="">None</MenuItem>
-    //       <MenuItem value="priceAsc">Price: Low to High</MenuItem>
-    //       <MenuItem value="priceDesc">Price: High to Low</MenuItem>
-    //       <MenuItem value="discountAsc">Discount: Low to High</MenuItem>
-    //       <MenuItem value="discountDesc">Discount: High to Low</MenuItem>
-    //     </Select>
-    //   </FormControl>
-    // </Box>
   );
 };
 
